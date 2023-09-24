51b4dc4a485d09c20c34f987c7a75b7c9d5b7924
302chanwoo <302chanwoo@users.noreply.github.com>
2023-09-24 09:32:11 +0000

    deploy: 856e40d2420012981232a408eafbb27855b637d4

---
+++ assets/index-f685675d.js
@@ -0,0 +1,17 @@
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const n="https://302chanwoo.github.io/react-router-page-action/assets/typescript-f6ead1af.svg",a="https://302chanwoo.github.io/react-router-page-action/vite.svg";function l(s){let o=0;const r=i=>{o=i,s.innerHTML=`count is ${o}`};s.addEventListener("click",()=>r(o+1)),r(0)}document.querySelector("#app").innerHTML=`
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${a}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${n}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;l(document.querySelector("#counter"));
