856e40d2420012981232a408eafbb27855b637d4
chanwoo hwang <302chanwoo@gmail.com>
2023-09-24 18:31:43 +0900

    vite config 추가

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  base: "https://302chanwoo.github.io/react-router-page-action",
+});
